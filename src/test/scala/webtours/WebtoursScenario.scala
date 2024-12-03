092d2b82b8beddf5326a2df1ab45788f57e1d958
maria.nenakhova <maria.nenakhova@ebs.ru>
2024-12-03 13:51:38 +0300

    feat: Реализованы покупка билета и возврат на страницу меню.

--- src/test/scala/webtours/WebtoursScenario.scala
+++ src/test/scala/webtours/WebtoursScenario.scala
@@ -4,6 +4,8 @@ import io.gatling.core.Predef._
 import io.gatling.core.structure._
 import webtours.Feeders.users
 
+import java.time.Year
+import java.util.UUID
 import scala.util.Random
 
 object WebtoursScenario {
@@ -29,12 +31,10 @@ class WebtoursScenario {
 
         val indexDepart = Random.nextInt(departureCities.size)
         val departureCity = departureCities(indexDepart)
-        println("depart = " + departureCity)
 
         arrivalCities = arrivalCities.filter(_ != departureCity)
         val indexArrival = Random.nextInt(arrivalCities.size)
         val arrivalCity = arrivalCities(indexArrival)
-        println("arrive = " + arrivalCity)
 
         val newSession = session.setAll(Map("depart" -> departureCity, "arrive" -> arrivalCity))
         newSession
@@ -48,11 +48,27 @@ class WebtoursScenario {
 
         val indexDepart = Random.nextInt(outboundFlights.size)
         val outboundFlight = outboundFlights(indexDepart)
-        println("outboundFlight = " + outboundFlight)
 
         val newSession = session.set("outboundFlight", outboundFlight)
         newSession
       })
+  }
+
+  val buyTicketGroup: ChainBuilder = group("buyTicketGroup") {
+    exec(Actions.paymentDetailsResult)
+      .exec(session => {
+        val creditCard = UUID.randomUUID.toString
+        val nextYear = Year.now.getValue + 1
+        val newSession = session.setAll(Map("creditCard" -> creditCard, "expDate" -> nextYear))
+        newSession
+      })
+      .exec(Actions.buyTicketResult)
+  }
+
+  val openHomePageGroup: ChainBuilder = group("openHomePageGroup") {
+    exec(Actions.openHomePage)
+      .exec(Actions.homeNavigationPageResult)
+      .exec(Actions.loginGetResult)
   }
 
   val scn: ScenarioBuilder = scenario("Webtours scenario")
@@ -60,4 +76,6 @@ class WebtoursScenario {
     .exec(loginGroup)
     .exec(chooseDirectionGroup)
     .exec(findFlightGroup)
+    .exec(buyTicketGroup)
+    .exec(openHomePageGroup)
 }
